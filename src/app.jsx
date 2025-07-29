cb94dccd3e045cb0c99a230a44ddd0c9a434b771
Sharon Shaji <71751576+shaderone@users.noreply.github.com>
2025-07-29 15:12:57 +0530

    create first component

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import './app.css'
+import Collection from "./components/collection";
+import Navbar from "./components/navbar";
+import Sidebar from "./components/sidebar";
+
+export default function App() {
+    return (
+        <>
+            <h1>hey</h1>
+        </>
+    );
+}
