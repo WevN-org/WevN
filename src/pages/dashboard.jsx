e4b702beb583b24b7be4a1d1be6bd59c891e61c3
Sharon Shaji <71751576+shaderone@users.noreply.github.com>
2025-08-07 01:12:21 +0530

    redesign UI with dark theme, modify sidebar styles

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,17 +2,16 @@ import Sidebar from "../components/sidebar";
 import Collection from "../components/collection";
 import '../styles/page_styles/dashboard.css';
 import { useState } from "react";
-import { collections } from "../../data/collection_data";
 
 export default function Dashboard() {
-    const [selectedCollection, setActiveCollection] = useState(collections[0].name);
+    const [selectedCollection, setActiveCollection] = useState("");
     function showCollection(CollectionName) {
-        console.log(` ${CollectionName} Collection clicked`);
+        //console.log(` ${CollectionName} Collection clicked`);
         setActiveCollection(CollectionName);
     }
     return (
         <div className="main">
-            <Sidebar onClick={showCollection} />
+            <Sidebar onClick={showCollection} selectedCollection={selectedCollection} />
             <Collection selectedCollection={selectedCollection} />
         </div>
     );
